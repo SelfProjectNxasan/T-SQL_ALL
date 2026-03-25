10490c2304dec09b268a6f03cd3588ba819af317
SelfProjectNxasan <unathinxasana14@gmail.com>
2026-03-26 00:33:51 +0200

    group anagrams

--- Data-Engineering-TSQL/Python/DSA/Arrays.py
+++ Data-Engineering-TSQL/Python/DSA/Arrays.py
@@ -31,7 +31,27 @@ def TwoSum(nums:list[int], target: int) -> list[int]:
             break
     return ret_vec
 
-target = int(input("Enter Target : "))
-vector = [4,5,6]
-indices = TwoSum(vector,target)
-print(f"VECTOR : {indices}")
+def groupAnagrams(strs: list[str])-> list[list[str]]:
+    grp_str = [[]]
+    for i in range(0,len(strs)):
+        new_group = []
+        exists = False
+        for k in range(0,len(grp_str)):
+            for j in range(0,len(grp_str[k])):
+                if(grp_str[k][j] == strs[i]):
+                    exists = True
+                    break
+            if(exists):
+                break
+        if(exists == False):
+            for x in range(0,len(strs)):
+                if(ValidAnagram(strs[i],strs[x]) == True and i != x):
+                    if (strs[i] not in (new_group)):
+                        new_group.append(strs[i])
+                    new_group.append(strs[x])
+            if(len(new_group) > 0):
+                grp_str.append(new_group)
+    return grp_str
+
+input_vector = ["act","pots","tops","cat","stop","hat"]
+print(f"GROUPED ANAGRAMS : {groupAnagrams(input_vector)}")
